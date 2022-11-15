3c00448ff9737af212d4abf43e38ee3433302c49
juhwan <asdcz@naver.com>
2022-11-15 23:27:08 +0900

    JPA

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,14 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
 
+    @Id @GeneratedValue (strategy = GenerationType.IDENTITY) // 자동으로 번호를 매겨주는것 (oracle 의 시퀀스기능)
     // field
     private Long id;
     private String name;
